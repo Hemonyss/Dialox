2876d61bffff09be45802114a1eae87a2934ee00
Hemonyss <hemonvoid@gmail.com>
2025-11-22 12:56:19 +0500

    feat: add token verification and refactor access token structure

--- core/engines/api_tokens_engine/models/access_token.py
+++ core/engines/api_tokens_engine/models/access_token.py
@@ -6,24 +6,45 @@ import time
 
 @dataclass
 class access_token:
-    # Sender ID
-    sen_id: int
-    # Recipient ID
-    rec_id: int
+    # Issuer
+    iss: str
+    # Subject
+    sub: str
+    # Audience
+    aud: str
     # Expiration time
     exp: int
-    # Creation time
+    # Issued at
     iat: int 
     # Access right
     right: int
 
-def create_access_token(sen_id: int, rec_id: int, right: int, exp: int = int(time.time()) + 900, tac: int = int(time.time())) -> Tuple:
-    access_token_dict = asdict(access_token(sen_id, rec_id, exp, tac, right))
+def create_access_token(iss: str, sub: str, aud: str, right: int, exp: int = int(time.time()) + 900, iat: int = int(time.time())) -> Tuple:
+    access_token_dict = asdict(access_token(iss, sub, aud, exp, iat, right))
     encode_key = token_urlsafe(32)
 
     access_token_encode = jwt.encode(payload=access_token_dict, key=encode_key, algorithm="HS256")
 
     return (encode_key, access_token_encode)
 
-def decode_access_token(token: str | bytes, key: str) -> Dict[str, Any]:
-    return jwt.decode(jwt=token, key=key, algorithms="HS256")
+def decode_access_token(token: str | bytes, key: str, aud: str) -> Dict[str, Any]:
+    try:
+        payload = jwt.decode(
+            token,
+            key,
+            algorithms="HS256",
+            options={"verify_exp": True},
+            audience=aud,
+            leeway=30
+        )
+
+        if payload["iss"] != payload["sub"]:
+            raise jwt.InvalidTokenError("Iss and sub not equal")
+
+        return payload
+    except jwt.ExpiredSignatureError:
+        return "The token is expired"
+    except jwt.InvalidSignatureError:
+        return "Incorrect signature"
+    except jwt.InvalidTokenError as e:
+        return f"Invalid token: {e}"
