28f2b6a3e3c691cb684748b777e265159b29f852
Hemonyss <hemonvoid@gmail.com>
2025-11-23 12:08:09 +0500

    feat: add deserialize_message function and improve stability

--- core/engines/message_converter_engine/models/message_converter.py
+++ core/engines/message_converter_engine/models/message_converter.py
@@ -22,14 +22,32 @@ class message:
     type: int
 
 def create_message(token: str, data: str | NDArray[uint8], time: int, chat_id: str, from_user: str, type: int, to_user: str | None = None) -> bytes:
-    # Converting data
-    raw_message = asdict(message(token, data, time, chat_id, from_user, to_user, type))
-    # Serialization message
-    packed_message = packb(raw_message)
-    # Compress message
-    compress_message = compress(packed_message)
+    try:
+        # Converting data
+        raw_message = asdict(message(token, data, time, chat_id, from_user, to_user, type))
+        # Serialization message
+        packed_message = packb(raw_message)
+        # Compress message
+        compress_message = compress(packed_message)
 
-    if len(compress_message) <= len(packed_message):
-        return b"\x01" + compress_message
-    
-    return b"\x00" + packed_message
+        if len(compress_message) <= len(packed_message):
+            return b"\x01" + compress_message
+        
+        return b"\x00" + packed_message
+    except Exception as e:
+        return f"Error: {e}"
+
+def deserialize_message(message: bytes):
+    try:
+        # Remove the flag byte
+        message_payload = message[1:]
+        # If message is not compressed
+        if message[0] == b"\x00":
+            # Return deserialize message
+            return unpackb(message_payload)
+        # If message is compressed, it is decompressed
+        decompressed_message = decompress(message_payload)
+        # Return deserialize message
+        return unpackb(decompressed_message)
+    except Exception as e:
+        return f"Error: {e}"
